eb7e1de48baa2f7710d9df5aa53c495936ac338c
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-07-12 09:41:24 +0200

    ajout de la page error/notfound

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -6,10 +6,13 @@ import logements from "../../datas/logement.json";
 const Gallery = () => {
   return (
     <div className="home_gallery">
+      {/* Pour chaque élément dans le tableau logements, une itération est effectuée pour créer une carte. */}
       {logements.map((logement) => {
         return (
           <article key={logement.id}>
+            {/* l'attribut key est necessaire pour que React puisse identifier chaque élément de manière unique */}
             <Link to={`/logement/${logement.id}`}>
+              {/* Chaque article contient un lien qui mène à une route spécifique au logement en utilisant son id. */}
               <Card image={logement.cover} title={logement.title} />
             </Link>
           </article>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import NotFoundError from "../components/notfound/NotFoundError";
 
 const NotFound = () => {
   return (
     <div className="notFound">
       <Header />
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <main>
+        <NotFoundError />
+      </main>
       <Footer />
     </div>
   );
